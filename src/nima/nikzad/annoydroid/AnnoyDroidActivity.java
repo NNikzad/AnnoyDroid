69360c230dcf1a387a4951b32876055c4566fe81
Nima Nikzad <nnikzad@ucla.edu>
2012-02-26 17:35:33 -0800

    Initial check in of new android project.

---
+++ src/nima/nikzad/annoydroid/AnnoyDroidActivity.java
@@ -0,0 +1,13 @@
+package nima.nikzad.annoydroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnnoyDroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
